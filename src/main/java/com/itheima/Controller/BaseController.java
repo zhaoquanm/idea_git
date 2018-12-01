faa69a14340dc6f82076c9cdb02a1607b7b48947
zqm <2269725605@qq.com>
2018-12-01 17:13:40 +0800

    git在本地版本库的管理

--- src/main/java/com/itheima/Controller/BaseController.java
+++ src/main/java/com/itheima/Controller/BaseController.java
@@ -2,4 +2,7 @@ package com.itheima.Controller;
 
 public class BaseController {
     //测试修该的文件
+    public static void main(String[] args) {
+        System.out.println("这个是将idea中的工程提交到gitbub中");
+    }
 }
