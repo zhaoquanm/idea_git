7f66c54bef1d25067da8cd84c602ac09d1098afa
zqm <2269725605@qq.com>
2018-12-01 17:33:38 +0800

    左侧修改的动作类的文件

--- src/main/java/com/itheima/Controller/BaseController.java
+++ src/main/java/com/itheima/Controller/BaseController.java
@@ -4,5 +4,6 @@ public class BaseController {
     //测试修该的文件
     public static void main(String[] args) {
         System.out.println("这个是将idea中的工程提交到gitbub中");
+        System.out.println("这个是左边的工程提交的动作类");
     }
 }
