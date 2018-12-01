38bd0bf835ccadab07c086c3178a6290060ae571
zqm <2269725605@qq.com>
2018-12-01 17:36:54 +0800

    这个是左边工程的修改的文件

--- src/main/java/com/itheima/Controller/BaseController.java
+++ src/main/java/com/itheima/Controller/BaseController.java
@@ -4,5 +4,6 @@ public class BaseController {
     //测试修该的文件
     public static void main(String[] args) {
         System.out.println("这个是将idea中的工程提交到gitbub中");
+        System.out.println("这个是右边的动作类修改的文件");
     }
 }
